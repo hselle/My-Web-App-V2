59c444461095058d59f159bcd970a5b0bdd48a11
Harrison Selle <harrison.selle@coloradocollege.edu>
2016-11-14 00:32:17 +0000

    third heroku push

--- app/controllers/game_controller.rb
+++
@@ -1,9 +0,0 @@
-require 'soundcloud'
-class GameController < ApplicationController
-
-  def game_params
-    params.require(:game).permit(:users, :songs, :winner, :loser, :genre, :start_time, :end_time)
-  end
-  
-  
-end
